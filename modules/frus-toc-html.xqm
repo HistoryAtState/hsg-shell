HistoryAtState/hsg-shell

commit eaa3b885a9b415f7d37b8b084e58eb741041d95a

Performance: directly call the generated tei-simple pm modules for transforming content, thus avoiding the overhead for dynamically resolving the pm for the current odd. The function to call is registered in $config:PUBLICATIONS. This leads to much faster response times under concurrent load. Downside: after changing one of the odds, you now have to call modules/rebuild.xql to re-generate the XQuery modules.
Requires update of tei-simple-pm xar.

modules/frus-toc-html.xqm
@@ -9,8 +9,8 @@ declare namespace tei="http://www.tei-c.org/ns/1.0";
 
 import module namespace templates="http://exist-db.org/xquery/templates";
 import module namespace config="http://history.state.gov/ns/site/hsg/config" at "config.xqm";
-import module namespace pmu="http://www.tei-c.org/tei-simple/xquery/util" at "/db/apps/tei-simple/content/util.xql";
-import module namespace odd="http://www.tei-c.org/tei-simple/odd2odd" at "/db/apps/tei-simple/content/odd2odd.xql";
+(:import module namespace pmu="http://www.tei-c.org/tei-simple/xquery/util" at "/db/apps/tei-simple/content/util.xql";:)
+(:import module namespace odd="http://www.tei-c.org/tei-simple/odd2odd" at "/db/apps/tei-simple/content/odd2odd.xql";:)
 
 declare
     %templates:wrap
@@ -119,8 +119,9 @@ declare function toc:remove-nodes-deep($nodes as node()*, $nodes-to-remove as no
 declare function toc:toc-head($model as map(*), $node as element(tei:head)) {
     let $head-sans-note := if ($node//tei:note) then toc:remove-nodes-deep($node, $node//tei:note) else $node
     return
-        pmu:process(odd:get-compiled($config:odd-source, $model?odd, $config:odd-compiled), $head-sans-note/node(), 
-            $config:odd-compiled, "web", "../generated", $config:module-config)
+        $model?odd($head-sans-note/node(), ())
+(:        pmu:process(odd:get-compiled($config:odd-source, $model?odd, $config:odd-compiled), $head-sans-note/node(), :)
+(:            $config:odd-compiled, "web", "../generated", $config:module-config):)
 };
 
 (: construct link to a FRUS div :)

modules/config.xqm
@@ -7,6 +7,9 @@ xquery version "3.1";
 module namespace config="http://history.state.gov/ns/site/hsg/config";
 import module namespace console="http://exist-db.org/xquery/console";
 
+import module namespace pm-frus='http://www.tei-c.org/tei-simple/models/frus.odd/module' at "../resources/odd/compiled/frus-web-module.xql";
+import module namespace pm-departmenthistory='http://www.tei-c.org/tei-simple/models/departmenthistory.odd/module' at "../resources/odd/compiled/departmenthistory-web-module.xql";
+
 declare namespace templates="http://exist-db.org/xquery/templates";
 
 declare namespace expath="http://expath.org/ns/pkg";
@@ -44,6 +47,11 @@ declare variable $config:odd-source := $config:odd-root || "/source";
 declare variable $config:odd-compiled := $config:odd-root || "/compiled";
 
 declare variable $config:odd := "frus.odd";
+
+(: Default transformation function: calls tei simple pm using frus.odd :)
+declare variable $config:odd-transform-default := function($xml, $parameters) {
+    pm-frus:transform($xml, $parameters)
+};
 
 declare variable $config:module-config := doc($config:odd-source || "/configuration.xml")/*;
 
@@ -93,6 +101,9 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:FRUS_VOLUMES_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/historicaldocuments/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": 
+                (: Called to transform content based on the odd using tei simple pm :)
+                function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "Historical Documents",
             "base-path": function($document-id, $section-id) { "frus/" || $document-id }
         },
@@ -102,6 +113,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:BUILDINGS_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/departmenthistory/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "Buildings - Department History",
             "base-path": function($document-id, $section-id) { "buildings" }
         },
@@ -117,6 +129,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:CONFERENCES_ARTICLES_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/conferences/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "Conferences",
             "base-path": function($document-id, $section-id) { "conferences" }
         },
@@ -138,6 +151,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:COUNTRIES_ARTICLES_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/countries/" || string-join(($document-id, $section-id), '/') },
             "odd": "departmenthistory.odd",
+            "transform": function($xml, $parameters) { pm-departmenthistory:transform($xml, $parameters) },
             "title": "Countries",
             "base-path": function($document-id, $section-id) { "countries" }
         },
@@ -147,6 +161,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:ARCHIVES_ARTICLES_COL || '/' || $document-id || '.xml')//tei:body },
             "html-href": function($document-id, $section-id) { "$app/countries/" || string-join(($document-id, $section-id), '/') },
             "odd": "departmenthistory.odd",
+            "transform": function($xml, $parameters) { pm-departmenthistory:transform($xml, $parameters) },
             "title": "World Wide Diplomatic Archives Indes"
         },
         "about": map {
@@ -197,6 +212,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:MILESTONES_COL|| '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/milestones/" || string-join(($document-id, $section-id), '/') },
             "odd": "departmenthistory.odd",
+            "transform": function($xml, $parameters) { pm-departmenthistory:transform($xml, $parameters) },
             "title": "Milestones in the History of U.S. Foreign Relations",
             "base-path": function($document-id, $section-id) { "milestones" }
         },
@@ -206,6 +222,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:SHORT_HISTORY_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/departmenthistory/" || string-join(($document-id, $section-id), '/') },
             "odd": "departmenthistory.odd",
+            "transform": function($xml, $parameters) { pm-departmenthistory:transform($xml, $parameters) },
             "title": "Short History - Department History",
             "base-path": function($document-id, $section-id) { "short-history" }
         },
@@ -215,6 +232,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:FAQ_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/about/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "FAQ - About Us"
         },
         "hac": map {
@@ -223,6 +241,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:HAC_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/about/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "Historical Advisory Committee - About Us"
         },
         "education": map {
@@ -231,6 +250,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:EDUCATION_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/about/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "Education Resources"
         },
         "frus-history-monograph": map {
@@ -239,6 +259,7 @@ declare variable $config:PUBLICATIONS :=
             "select-section": function($document-id, $section-id) { doc($config:FRUS_HISTORY_MONOGRAPH_COL || '/' || $document-id || '.xml')/id($section-id) },
             "html-href": function($document-id, $section-id) { "$app/historicaldocuments/" || string-join(($document-id, $section-id), '/') },
             "odd": "frus.odd",
+            "transform": function($xml, $parameters) { pm-frus:transform($xml, $parameters) },
             "title": "History of the Foreign Relations Series",
             "base-path": function($document-id, $section-id) { "frus-history" }
         }

modules/frus-ajax.xql
@@ -32,7 +32,7 @@ let $volume := if ($volume) then $volume else $publication-id (: TODO see above 
 let $xml := pages:load-xml($publication-id, $volume, $id, "div")
 return
     if ($xml) then
-        let $odd := if ($publication-id) then map:get($config:PUBLICATIONS, $publication-id)?odd else $config:odd
+        let $odd := if ($publication-id) then map:get($config:PUBLICATIONS, $publication-id)?transform else $config:odd-transform-default
         let $parent := $xml/ancestor::tei:div[not(*[1] instance of element(tei:div))][1]
         let $prevDiv := $xml/preceding::tei:div[not(@xml:id = $config:IGNORED_DIVS)][1]
         let $prev := pages:get-previous(
@@ -58,7 +58,6 @@ return
         return
             map {
                 "doc": $doc,
-                "odd": $odd,
                 "next": 
                     if ($next) then 
                         $next/@xml:id/string()

modules/pages.xqm
@@ -11,8 +11,8 @@ declare namespace expath="http://expath.org/ns/pkg";
 
 import module namespace templates="http://exist-db.org/xquery/templates";
 import module namespace config="http://history.state.gov/ns/site/hsg/config" at "config.xqm";
-import module namespace pmu="http://www.tei-c.org/tei-simple/xquery/util" at "/db/apps/tei-simple/content/util.xql";
-import module namespace odd="http://www.tei-c.org/tei-simple/odd2odd" at "/db/apps/tei-simple/content/odd2odd.xql";
+(:import module namespace pmu="http://www.tei-c.org/tei-simple/xquery/util" at "/db/apps/tei-simple/content/util.xql";:)
+(:import module namespace odd="http://www.tei-c.org/tei-simple/odd2odd" at "/db/apps/tei-simple/content/odd2odd.xql";:)
 import module namespace console="http://exist-db.org/xquery/console" at "java:org.exist.console.xquery.ConsoleModule";
 
 declare variable $pages:app-root := request:get-context-path() || substring-after($config:app-root, "/db");
@@ -41,7 +41,7 @@ function pages:load($node as node(), $model as map(*), $publication-id as xs:str
                     map:get($config:PUBLICATIONS, $publication-id)?base-path($document-id, $section-id) 
                 else ()
             else (),
-        "odd": if (exists($publication-id)) then map:get($config:PUBLICATIONS, $publication-id)?odd else $config:odd
+        "odd": if (exists($publication-id)) then map:get($config:PUBLICATIONS, $publication-id)?transform else $config:odd-transform-default
     }
     let $html := templates:process($node/*, map:new(($model, $content)))
     (: without an entry in $config:PUBLICATIONS and a publication-id parameter from controller.xql, 
@@ -136,14 +136,16 @@ function pages:header($node as node(), $model as map(*)) {
         pages:process-content($model?odd, $header)
 };
 
-declare function pages:process-content($odd as xs:string, $xml as element()*) {
+declare function pages:process-content($odd as function(*), $xml as element()*) {
     pages:process-content($odd, $xml, ())
 };
 
-declare function pages:process-content($odd as xs:string, $xml as element()*, $parameters as map(*)?) {
+declare function pages:process-content($odd as function(*), $xml as element()*, $parameters as map(*)?) {
+(:    console:log("Processing content using odd: " || $odd),:)
 	let $html :=
-        pmu:process(odd:get-compiled($config:odd-source, $odd, $config:odd-compiled), $xml, $config:odd-compiled, "web", "../generated", 
-            $config:module-config, $parameters)
+	    $odd($xml, $parameters)
+(:        pmu:process(odd:get-compiled($config:odd-source, $odd, $config:odd-compiled), $xml, $config:odd-compiled, "web", "../generated", :)
+(:            $config:module-config, $parameters):)
     let $content := pages:clean-footnotes($html)
     let $class := if ($html//*[@class = ('margin-note')]) then "margin-right" else ()
     return
@@ -302,8 +304,7 @@ function pages:navigation-link($node as node(), $model as map(*), $direction as 
     else if ($model($direction)) then
         <a data-doc="{util:document-name($model($direction))}"
             data-root="{util:node-id($model($direction))}"
-            data-current="{util:node-id($model('div'))}"
-            data-odd="{$model?odd}">
+            data-current="{util:node-id($model('div'))}">
         {
             $node/@* except $node/@href,
             let $id := $model($direction)/@xml:id

resources/odd/compiled/departmenthistory-web-module.xql
@@ -1,0 +1,16 @@
+module namespace pml='http://www.tei-c.org/tei-simple/models/departmenthistory.odd/module';
+
+import module namespace m='http://www.tei-c.org/tei-simple/models/departmenthistory.odd' at '/db/apps/hsg-shell/resources/odd/compiled/departmenthistory-web.xql';
+
+(: Generated library module to be directly imported into code which
+ : needs to transform TEI nodes using the ODD this module is based on.
+ :)
+declare function pml:transform($xml as node()*, $parameters as map(*)?) {
+
+   let $options := map {
+       "styles": ["../generated/departmenthistory.css"],
+       "collection": "/db/apps/hsg-shell/resources/odd/compiled",
+       "parameters": $parameters
+   }
+   return m:transform($options, $xml)
+};

resources/odd/compiled/frus-web-module.xql
@@ -1,0 +1,16 @@
+module namespace pml='http://www.tei-c.org/tei-simple/models/frus.odd/module';
+
+import module namespace m='http://www.tei-c.org/tei-simple/models/frus.odd' at '/db/apps/hsg-shell/resources/odd/compiled/frus-web.xql';
+
+(: Generated library module to be directly imported into code which
+ : needs to transform TEI nodes using the ODD this module is based on.
+ :)
+declare function pml:transform($xml as node()*, $parameters as map(*)?) {
+
+   let $options := map {
+       "styles": ["../generated/frus.css"],
+       "collection": "/db/apps/hsg-shell/resources/odd/compiled",
+       "parameters": $parameters
+   }
+   return m:transform($options, $xml)
+};

commit 473fd204eb354b2f21e1c98c6dd4d00d77f88f27

Fix display of table of contents for inner chapters. Got broken by eaa3b885a9b415f7d37b8b084e58eb741041d95a

modules/frus-toc-html.xqm
@@ -9,6 +9,7 @@ declare namespace tei="http://www.tei-c.org/ns/1.0";
 
 import module namespace templates="http://exist-db.org/xquery/templates";
 import module namespace config="http://history.state.gov/ns/site/hsg/config" at "config.xqm";
+
 (:import module namespace pmu="http://www.tei-c.org/tei-simple/xquery/util" at "/db/apps/tei-simple/content/util.xql";:)
 (:import module namespace odd="http://www.tei-c.org/tei-simple/odd2odd" at "/db/apps/tei-simple/content/odd2odd.xql";:)
 
@@ -89,7 +90,7 @@ declare function toc:toc-div($model as map(*), $node as element(tei:div), $curre
                         else
                             concat('s ', $first, '-', $last)
                     , ')'
-                    )
+                )
             else 
                 <ul>
                 {
@@ -119,7 +120,11 @@ declare function toc:remove-nodes-deep($nodes as node()*, $nodes-to-remove as no
 declare function toc:toc-head($model as map(*), $node as element(tei:head)) {
     let $head-sans-note := if ($node//tei:note) then toc:remove-nodes-deep($node, $node//tei:note) else $node
     return
-        $model?odd($head-sans-note/node(), ())
+        (: Check if we're called via tei-simple pm or templating :)
+        if (exists($model?apply-children)) then
+            $model?apply-children($model, $node, $head-sans-note/node())
+        else
+            $model?odd($head-sans-note/node(), ())
 (:        pmu:process(odd:get-compiled($config:odd-source, $model?odd, $config:odd-compiled), $head-sans-note/node(), :)
 (:            $config:odd-compiled, "web", "../generated", $config:module-config):)
 };
@@ -143,6 +148,9 @@ declare function toc:href($publication-id as xs:string, $document-id as xs:strin
     )
 };
 
+(:~
+ : Generate table of contents for divs. Called via tei-simple pm.
+ :)
 declare function toc:document-list($config as map(*), $node as element(tei:div), $class) {
     let $head := $node/tei:head[1]
     let $head-sans-notes := toc:remove-nodes-deep($head, $head//tei:note)
@@ -212,11 +220,7 @@ declare function toc:document-list($config as map(*), $node as element(tei:div),
                     <h4>Contents</h4>
                     <div style="padding-left: 1em">
                         <div class="toc-inner">
-                            <ul>{
-                                let $divs := $node//tei:div except $node//tei:div//tei:div
-                                return
-                                    $config?apply-children($config, $node, $divs)
-                            }</ul>
+                            <ul>{ toc:toc-inner($config, $node, false()) }</ul>
                         </div>
                     </div>
                 </div>