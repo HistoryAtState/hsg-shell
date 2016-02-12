2d6d086940297a22e5824df79696972bb86b29ad
Juri Leino <github@line-o.de>
2016-02-12 12:53:12 +0100

    add tasks for local development
    * add a gulpfile with first `watch` and `deploy` tasks working
    * add package.json with development decencies
    
    TODO: add preprocessing steps to js and css watchers
    NOTE: License is unclear.

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp'),
+    exist = require('gulp-exist'),
+    watch = require('gulp-watch')
+
+exist.createClient({
+    host: 'localhost',
+    port: '8080',
+    path: '/exist/xmlrpc',
+    basic_auth: { user: 'admin', pass: '' }
+})
+
+var existConfiguration = {
+    target: '/db/apps/hsg-shell/',
+    retry: true
+}
+
+function deployTaskFor(sourcePath) {
+    return function () {
+        return gulp.src(sourcePath, {base: '.'})
+            .pipe(exist.newer(existConfiguration))
+            .pipe(exist.dest(existConfiguration))
+    }
+}
+
+function watchTaskFor(sourcePath) {
+    return function () {
+        return gulp.src(sourcePath, {base: '.'})
+            .pipe(watch(sourcePath))
+            .pipe(exist.dest(existConfiguration))
+    }
+}
+
+gulp.task('watch.modules', watchTaskFor('modules/*'))
+gulp.task('watch.templates', watchTaskFor('templates/*'))
+gulp.task('watch.resources', watchTaskFor('resources/**/*'))
+gulp.task('watch.pages', watchTaskFor('pages/**/*'))
+
+gulp.task('watch', [
+    'watch.modules',
+    'watch.templates',
+    'watch.resources',
+    'watch.pages'
+])
+
+gulp.task('deploy.modules', deployTaskFor('modules/*'))
+gulp.task('deploy.templates', deployTaskFor('templates/*'))
+gulp.task('deploy.resources', deployTaskFor('resources/**/*'))
+gulp.task('deploy.pages', deployTaskFor('pages/**/*'))
+
+gulp.task('default', [
+    'deploy.modules',
+    'deploy.templates',
+    'deploy.resources',
+    'deploy.pages'
+])
+
+// TODO package deployment fails due to missing build folder in eXistDB
+//gulp.task('deploy.package', deployTaskFor('build/*.xar'))
+//gulp.task('default', ['deploy.package'])
