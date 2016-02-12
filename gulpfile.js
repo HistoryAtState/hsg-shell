9efa8ae7461e5fe436ddc981898e2fe7d6ce6fc6
Juri Leino <github@line-o.de>
2016-02-12 18:30:25 +0100

    gulp style task working

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass')
 
-
 exist.createClient({
     host: 'localhost',
     port: '8080',
@@ -18,56 +17,20 @@ var existConfiguration = {
     retry: true
 }
 
-function deployTaskFor(sourcePath) {
-    return function () {
-        return gulp.src(sourcePath, {base: '.'})
-            .pipe(exist.newer(existConfiguration))
-            .pipe(exist.dest(existConfiguration))
-    }
-}
-
-function watchTaskFor(sourcePath) {
-    return function () {
-        return gulp.src(sourcePath, {base: '.'})
-            .pipe(watch(sourcePath))
-            .pipe(exist.dest(existConfiguration))
-    }
-}
-
-gulp.task('watch.modules', watchTaskFor('./modules/*'))
-gulp.task('watch.templates', watchTaskFor('./templates/*'))
-gulp.task('watch.resources', watchTaskFor('./resources/**/*'))
-gulp.task('watch.pages', watchTaskFor('./pages/**/*'))
-
-gulp.task('sass', function () {
+gulp.task('styles:build', function () {
     return gulp.src('./resources/sass/main.scss', {base: '.'})
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./resources/css'))
 })
 
-gulp.task('watch.styles', ['watch.resources'], function () {
-    gulp.watch('./resources/sass/**/*.scss', ['sass'])
+gulp.task('styles:deploy', function () {
+    return gulp.src('./resources/css/*.css', {base: '.'})
+        .pipe(exist.dest(existConfiguration))
 })
 
-gulp.task('watch', [
-    'watch.modules',
-    'watch.templates',
-    'watch.resources',
-    'watch.pages'
-])
+gulp.task('styles:watch', function () {
+    gulp.watch('./resources/css/*.css', ['styles:deploy'])
+    gulp.watch('./resources/sass/**/*.scss', ['styles:build'])
+})
 
-gulp.task('deploy.modules', deployTaskFor('./modules/*'))
-gulp.task('deploy.templates', deployTaskFor('./templates/*'))
-gulp.task('deploy.resources', deployTaskFor('./resources/**/*'))
-gulp.task('deploy.pages', deployTaskFor('./pages/**/*'))
-
-gulp.task('default', [
-    'deploy.modules',
-    'deploy.templates',
-    'deploy.resources',
-    'deploy.pages'
-])
-
-// TODO package deployment fails due to missing build folder in eXistDB
-//gulp.task('deploy.package', deployTaskFor('build/*.xar'))
-//gulp.task('default', ['deploy.package'])
+gulp.task('default', ['styles:watch'])
