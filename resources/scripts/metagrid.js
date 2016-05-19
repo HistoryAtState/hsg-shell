19bf06dcae65014aa04008a6885bcdc1e59794ab
plutonik <plutonik@gmx.net>
2016-05-19 16:24:33 +0200

    fix (metagrid): Only display widget if data is available #228
    The html is built dynamically with JS if data was returned by Metagrid
    API.
    
    Fixes https://github.com/eXistSolutions/hsg-shell/issues/228

--- resources/scripts/metagrid.js
+++ resources/scripts/metagrid.js
@@ -7,12 +7,19 @@ $(document).ready(function () {
 
     $.getJSON('https://api.metagrid.ch/widget/history-state/person/' + personId[1] + '.json?lang=en&include=true&jsoncallback=?', function (data) {
         if (data[0]) {
-            $("#metagrid").show();
+            $("#metagrid-wrapper").html('<div id="metagrid" class="hsg-panel">' +
+                '<div class="hsg-panel-heading">' +
+                '<h2 id="metagrid-headline" class="hsg-sidebar-title">Links from Metagrid.ch (beta)</h2>' +
+                '</div>' +
+                '<ul id="metagrid-container" class="hsg-list-group"></ul>' +
+                '</div>');
 
             $.each(data[0], function (index, value) {
                 $('<li><span class="hsg-external-link" aria-hidden="true"></span>').attr({
                     class: 'metagrid hsg-list-group-item'
-                }).append('<a>').appendTo('#metagrid-container');
+                }).append('<a>').attr({
+                    target: '_blank'
+                }).appendTo('#metagrid-container');
                 $('#metagrid-container li:last-child a').attr({
                     title: value.short_description, href: value.url
                 }).text(index);
