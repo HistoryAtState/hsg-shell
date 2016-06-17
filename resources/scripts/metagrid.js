777a0d8f834ab893c18c08256e39b18ff0e63790
plutonik <plutonik@gmx.net>
2016-06-17 15:51:55 +0200

    fix(metagrid): Remove superfluous elements
    * delete „external-link“ class, because Metagrid links a filtered by
    CSS attribute selectors now
    * delete wrong condition for stopping metagrid.js

--- resources/scripts/metagrid.js
+++ resources/scripts/metagrid.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
 
     var personId = window.location.href.match(/\/departmenthistory\/people\/([^/#]+)$/i);
-    if (!personID) {
-        return;
-    };
 
     $.getJSON('https://api.metagrid.ch/widget/history-state/person/' + personId[1] + '.json?lang=en&include=true&jsoncallback=?', function (data) {
         if (data[0]) {
@@ -15,7 +12,7 @@ $(document).ready(function () {
                 '</div>');
 
             $.each(data[0], function (index, value) {
-                $('<li><span class="hsg-external-link" aria-hidden="true"></span>').attr({
+                $('<li><span aria-hidden="true"></span>').attr({
                     class: 'metagrid hsg-list-group-item'
                 }).append('<a>').appendTo('#metagrid-container');
                 $('#metagrid-container li:last-child a').attr({
