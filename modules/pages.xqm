HistoryAtState/hsg-shell

commit 1dfd9a33bf06fe2c70dabf09bb6dc9a3233eb4f0

Small optimizations for slightly faster lookup of next/previous page.

modules/pages.xqm
@@ -222,9 +222,9 @@ declare function pages:process-content($odd as function(*), $xml as element()*, 
 (:        pmu:process(odd:get-compiled($config:odd-source, $odd, $config:odd-compiled), $xml, $config:odd-compiled, "web", "../generated", :)
 (:            $config:module-config, $parameters):)
     let $content := pages:clean-footnotes($html)
-    let $class := if ($html//*[@class = ('margin-note')]) then "margin-right" else ()
+    (: let $class := if ($html//*[@class = ('margin-note')]) then "margin-right" else () :)
     return
-        <div class="content {$class}">
+        <div class="content">
             {
             $content
             ,
@@ -332,18 +332,18 @@ declare function pages:get-next($div as element()) {
     else if ($div/tei:div[@xml:id]) then
         $div/tei:div[@xml:id][1]
     else
-        $div/following::tei:div[@xml:id][not(@xml:id = $config:IGNORED_DIVS)][1]
+        ($div/following::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[1]
 };
 
 declare function pages:get-previous($div as element()?) {
     if ($div/self::tei:pb) then
         $div/preceding::tei:pb[1]
-    else if ($div/preceding-sibling::tei:div[@xml:id][not(@xml:id = $config:IGNORED_DIVS)][not(tei:div/@type)]) then
-        $div/preceding-sibling::tei:div[@xml:id][not(@xml:id = $config:IGNORED_DIVS)][1]
+    else if (($div/preceding-sibling::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[not(tei:div/@type)]) then
+        ($div/preceding-sibling::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[1]
     else
         (
             $div/ancestor::tei:div[@type = ('compilation', 'chapter', 'subchapter', 'section', 'part')][tei:div/@type][1],
-            $div/preceding::tei:div[@xml:id][not(@xml:id = $config:IGNORED_DIVS)][1]
+            ($div/preceding::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[1]
         )[1]
 };
 

commit 1be98eab0a9ec3237e096dfc3f0962a6b5628270

Fix back button navigation. Closes #204.
The bug was introduced in
https://github.com/eXistSolutions/hsg-shell/commit/1dfd9a33bf06fe2c70dab
f09bb6dc9a3233eb4f0.
Specifically, that commit’s addition of parentheses surrounding the
preceding-sibling node set reset the ordering of these nodes to
document order, not the axis order.

modules/pages.xqm
@@ -330,7 +330,7 @@ declare function pages:get-previous($div as element()?) {
     if ($div/self::tei:pb) then
         $div/preceding::tei:pb[1]
     else if (($div/preceding-sibling::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[not(tei:div/@type)]) then
-        ($div/preceding-sibling::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[1]
+        ($div/preceding-sibling::tei:div[@xml:id] except $div/id($config:IGNORED_DIVS))[last()]
     else
         (
             $div/ancestor::tei:div[@type = ('compilation', 'chapter', 'subchapter', 'section', 'part')][tei:div/@type][1],