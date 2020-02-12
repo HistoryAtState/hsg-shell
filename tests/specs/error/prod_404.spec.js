44c7dcbaa97b7a276c909a6075a7d376489b8d20
plutonik <plutonik@gmx.net>
2020-02-12 17:44:39 +0100

    chore(test): Delete comments

--- tests/specs/error/prod_404.spec.js
+++ tests/specs/error/prod_404.spec.js
@@ -3,13 +3,11 @@
  */
 
 const Page = require('../../pageobjects/Page');
-//const assert = require('assert')
 const { assert } = require('chai');
 
 describe('Requesting a non existing page', () => {
   it('should redirect to the 404 error page', () => {
     Page.open('asdfg');
     assert.include(Page.getTitle(), 'Page Not found');
-    console.log("Hallo")
   });
 });
