402affc43f856322a545abb554f8944799314af3
Alexandra von Criegern <alexandra@evolvedbinary.com>
2022-10-17 18:18:26 +0200

    fix(OSD): Customize OSD main JS to use SVG
    There is currently no other way to customize openseadragon icons than to replace static filenames in `openseadrgon.js` and replace the listed icon filenames ending with `.png` with `.svg`.
    Since Openseadragon is installed via npm, we have to manually replace the icon filenames and provide the according icons in a separate folder, and its path has to be provided in the Openseadragon configuration in `app.js` under parameter `prefixUrl`
    -> e.g: `prefixUrl: "resources/images/OSD-icons/"`
    
    In the future, this step could be done via `gulp-replace`, where we can replace those custom icons in `openseadragon.js` after copying into the resources folder and before concatenating it with the other scripts.
    
    See https://openseadragon.github.io/#plugins -> Icon Packs

--- resources/scripts/vendor/openseadragon/openseadragon.js
+++ resources/scripts/vendor/openseadragon/openseadragon.js
@@ -1328,58 +1328,58 @@ function OpenSeadragon( options ){
             prefixUrl:              "/images/",
             navImages: {
                 zoomIn: {
-                    REST:   'zoomin_rest.png',
-                    GROUP:  'zoomin_grouphover.png',
-                    HOVER:  'zoomin_hover.png',
-                    DOWN:   'zoomin_pressed.png'
+                    REST:   'zoomin_rest.svg',
+                    GROUP:  'zoomin_grouphover.svg',
+                    HOVER:  'zoomin_hover.svg',
+                    DOWN:   'zoomin_pressed.svg'
                 },
                 zoomOut: {
-                    REST:   'zoomout_rest.png',
-                    GROUP:  'zoomout_grouphover.png',
-                    HOVER:  'zoomout_hover.png',
-                    DOWN:   'zoomout_pressed.png'
+                    REST:   'zoomout_rest.svg',
+                    GROUP:  'zoomout_grouphover.svg',
+                    HOVER:  'zoomout_hover.svg',
+                    DOWN:   'zoomout_pressed.svg'
                 },
                 home: {
-                    REST:   'home_rest.png',
-                    GROUP:  'home_grouphover.png',
-                    HOVER:  'home_hover.png',
-                    DOWN:   'home_pressed.png'
+                    REST:   'home_rest.svg',
+                    GROUP:  'home_grouphover.svg',
+                    HOVER:  'home_hover.svg',
+                    DOWN:   'home_pressed.svg'
                 },
                 fullpage: {
-                    REST:   'fullpage_rest.png',
-                    GROUP:  'fullpage_grouphover.png',
-                    HOVER:  'fullpage_hover.png',
-                    DOWN:   'fullpage_pressed.png'
+                    REST:   'fullpage_rest.svg',
+                    GROUP:  'fullpage_grouphover.svg',
+                    HOVER:  'fullpage_hover.svg',
+                    DOWN:   'fullpage_pressed.svg'
                 },
                 rotateleft: {
-                    REST:   'rotateleft_rest.png',
-                    GROUP:  'rotateleft_grouphover.png',
-                    HOVER:  'rotateleft_hover.png',
-                    DOWN:   'rotateleft_pressed.png'
+                    REST:   'rotateleft_rest.svg',
+                    GROUP:  'rotateleft_grouphover.svg',
+                    HOVER:  'rotateleft_hover.svg',
+                    DOWN:   'rotateleft_pressed.svg'
                 },
                 rotateright: {
-                    REST:   'rotateright_rest.png',
-                    GROUP:  'rotateright_grouphover.png',
-                    HOVER:  'rotateright_hover.png',
-                    DOWN:   'rotateright_pressed.png'
+                    REST:   'rotateright_rest.svg',
+                    GROUP:  'rotateright_grouphover.svg',
+                    HOVER:  'rotateright_hover.svg',
+                    DOWN:   'rotateright_pressed.svg'
                 },
                 flip: { // Flip icon designed by Yaroslav Samoylov from the Noun Project and modified by Nelson Campos ncampos@criteriamarathon.com, https://thenounproject.com/term/flip/136289/
-                    REST:   'flip_rest.png',
-                    GROUP:  'flip_grouphover.png',
-                    HOVER:  'flip_hover.png',
-                    DOWN:   'flip_pressed.png'
+                    REST:   'flip_rest.svg',
+                    GROUP:  'flip_grouphover.svg',
+                    HOVER:  'flip_hover.svg',
+                    DOWN:   'flip_pressed.svg'
                 },
                 previous: {
-                    REST:   'previous_rest.png',
-                    GROUP:  'previous_grouphover.png',
-                    HOVER:  'previous_hover.png',
-                    DOWN:   'previous_pressed.png'
+                    REST:   'previous_rest.svg',
+                    GROUP:  'previous_grouphover.svg',
+                    HOVER:  'previous_hover.svg',
+                    DOWN:   'previous_pressed.svg'
                 },
                 next: {
-                    REST:   'next_rest.png',
-                    GROUP:  'next_grouphover.png',
-                    HOVER:  'next_hover.png',
-                    DOWN:   'next_pressed.png'
+                    REST:   'next_rest.svg',
+                    GROUP:  'next_grouphover.svg',
+                    HOVER:  'next_hover.svg',
+                    DOWN:   'next_pressed.svg'
                 }
             },
 
