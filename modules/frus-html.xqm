HistoryAtState/hsg-shell

commit 5f446adcaee3ba542a2244be6162ecc91f9d5655

Provide fallback for volumes with minimal titles
e.g., frus1918 - see https://github.com/HistoryAtState/frus/commit/3ab8e4bd852b7ccf46fe35487e153fa48a092a2c

modules/frus-html.xqm
@@ -223,7 +223,14 @@ declare
     %templates:wrap
 function fh:series-volumes($node as node(), $model as map(*)) {
     for $vol in $model?volumes
-    let $title := string-join(for $title in ($vol/title[@type='volume'], $vol/title[@type='volume-number'])[. ne ''] return $title, ', ')
+    let $title := 
+        string-join(
+            (
+                ($vol/title[@type='volume'], $vol/title[@type='volume-number'])[. ne ''], 
+                $vol/title[@type='sub-series']
+            )[1], 
+            ', '
+        )
     let $vol-id := $vol/@id
     let $publication-status := $vol/publication-status
     order by $vol-id

commit 0cd424a7d694ad0759958539e679fcf9875b3a08

Fix regression in display of volume & part #s
… in administration listings. The regression was inadvertently introduced in https://github.com/HistoryAtState/hsg-shell/commit/5f446adcaee3ba542a2244be6162ecc91f9d5655

modules/frus-html.xqm
@@ -223,14 +223,15 @@ declare
     %templates:wrap
 function fh:series-volumes($node as node(), $model as map(*)) {
     for $vol in $model?volumes
-    let $title := 
-        string-join(
-            (
-                ($vol/title[@type='volume'], $vol/title[@type='volume-number'])[. ne ''], 
-                $vol/title[@type='sub-series']
-            )[1], 
-            ', '
-        )
+    let $title :=
+        (: catch outliers like frus1918, which have no volume title or number :)
+        if ($vol/title[@type eq "volume"] eq "" and $vol/title[@type eq "volume-number"] eq "") then
+            $vol/title[@type eq "sub-series"]
+        else
+            string-join(
+                ($vol/title[@type eq "volume"], $vol/title[@type eq "volume-number"])[. ne ""],
+                ', '
+            )
     let $vol-id := $vol/@id
     let $publication-status := $vol/publication-status
     order by $vol-id