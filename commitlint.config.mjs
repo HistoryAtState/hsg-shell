be6cf66dbe0b2970ea313a993f782b845e34b74d
duncdrum <d.paterson@me.com>
2026-03-03 16:31:25 +0100

    chore(commitlint): update line length rules for commit messages
    - Increase body-max-line-length from 200 to 250
    - Add footer-max-line-length rule set to 150

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,6 @@
 export default { 
   extends: ['@commitlint/config-conventional'], 
   rules: {
-      'body-max-line-length': [1, 'always', 200]
-    } 
+      'body-max-line-length': [1, 'always', 250],
+      'footer-max-line-length': [1, 'always', 150]    } 
 }
